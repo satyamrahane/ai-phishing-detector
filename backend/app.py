276e85de182cecfad1925c7ce9d8abd6cfe377cd
Satyam Rahane <satyam.rahane2@gmail.com>
2026-03-05 23:11:54 +0530

    Added CORS support for frontend integration

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,8 @@ from routes import scan_route
 from flask_cors import CORS
 
 app = Flask(__name__)
-# Enable CORS for the frontend to be able to make requests to this backend later
+
+# Enable CORS (Cross-Origin Resource Sharing) for the entire application
 CORS(app)
 
 app.register_blueprint(scan_route)
